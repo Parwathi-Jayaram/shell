d9b7c175998d4ba159ac9a9e4be5a58e099f87b1
Parwathi-Jayaram <parwathijayaram1@gmail.com>
2026-09-11 19:58:35 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,79 @@
+#include <cstdlib>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <unistd.h>
 
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
   while (true) {
     std::cout << "$ ";
+
     std::string command;
     std::getline(std::cin, command);
-    if (command == "exit") {
+
+    if (command == "exit 0") {
       break;
     }
+
     if (command.rfind("echo ", 0) == 0) {
       std::cout << command.substr(5) << "\n";
-    } else if (command.rfind("type ", 0) == 0) {
-      if (command.substr(5) == "echo" || command.substr(5) == "exit" ||
-          command.substr(5) == "type") {
-        std::cout << command.substr(5) << " is a shell builtin\n";
+    }
+
+    else if (command.rfind("type ", 0) == 0) {
+      std::string cmd = command.substr(5);
+
+      if (cmd == "echo" || cmd == "exit" || cmd == "type") {
+        std::cout << cmd << " is a shell builtin\n";
       } else {
         std::string pathvar = std::getenv("PATH");
         std::istringstream path_stream(pathvar);
         std::string pathsplit;
-        int found = 0;
+        bool found = false;
+
         while (std::getline(path_stream, pathsplit, ':')) {
-          std::string filepath = pathsplit + '/' + command.substr(5);
+          std::string filepath = pathsplit + "/" + cmd;
+
           if (access(filepath.c_str(), X_OK) == 0) {
-            std::cout << command.substr(5) << " is " << filepath << std::endl;
-            found = 1;
+            std::cout << cmd << " is " << filepath << "\n";
+            found = true;
             break;
           }
         }
-        if (found == 0) {
-          std::cout << command.substr(5) << ": not found\n";
+
+        if (!found) {
+          std::cout << cmd << ": not found\n";
         }
       }
-    } else {
-      std::cout << command << ": command not found\n";
+    }
+
+    else {
+      std::istringstream iss(command);
+      std::string cmd;
+      iss >> cmd;
+
+      std::string pathvar = std::getenv("PATH");
+      std::istringstream path_stream(pathvar);
+      std::string pathsplit;
+      bool found = false;
+
+      while (std::getline(path_stream, pathsplit, ':')) {
+        std::string filepath = pathsplit + "/" + cmd;
+
+        if (access(filepath.c_str(), X_OK) == 0) {
+          found = true;
+          std::system(command.c_str());
+          break;
+        }
+      }
+
+      if (!found) {
+        std::cout << cmd << ": command not found\n";
+      }
     }
   }
+
   return 0;
 }
